60027462769653d56ce7956e094613569b50d20f
Jin Longbin <51145547+jinlongbin@users.noreply.github.com>
2022-11-17 15:32:55 +0900

    input byte-string

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import os
+from io import BytesIO
 import cv2
 import numpy as np
 from tqdm import tqdm
+from PIL import Image
 import face_recognition
 import pickle
 import dlib
+import base64
 
 dlib.DLIB_USE_CUDA=True
 
 
-def face_detection(img_path:str, upsample:int):
+def load_base64(base64_string, mode='RGB'):
+    """
+    이미지 파일을 bytes에서 numpy array로 변환
+    """
+    image = Image.open(BytesIO(base64.b64decode(base64_string)))
+    if mode=='RGB':
+        image = image.convert('RGB')
+    elif mode == 'BGR':
+        image = image.convert('RGB')
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    return np.array(image)
+
+
+def face_detection(base64_string:bytes, upsample:int):
     '''한장의 이미지에서 얼굴 위치와 얼굴 특징 (128차원 벡터) 추출'''
 
-    image = face_recognition.load_image_file(img_path)
+    image = load_base64(base64_string, mode='RGB')
+
     face_location = face_recognition.face_locations(image, number_of_times_to_upsample=upsample, model='cnn')
     face_encoding = face_recognition.face_encodings(image, known_face_locations=face_location, num_jitters=1, model='small')
     return face_location, face_encoding
 
 
-def save_face_par(img_folder:str, child_names:list, data_folder:str, class_name:str):
+def save_face_par(base64_string:bytes, child_ids:str, data_folder:str, class_name:str):
     '''
     부모가 처음 앱에 가입할 때 본인 아이 사진을 업로드 하면 
     그 사진들에서 얼굴을 검출하고 128차원 벡터로 추출하여 파일로 저장
-    업로드 된 사진 들은 img_folder에 저장됨
-    사진마다 하나의 얼굴이 검출되어야 함
-    child_names list에 하나의 원소가 있으면 img_folder에 사진들이 전부 해당 유아 사진
-    child_names list에 원소가 img_folder에 있는 사진개수 만큼 있으면 여러 유아 개별 사진
+    이미지를 byte-string 형식으로 입력
+    사진에서 하나의 얼굴이 검출되어야 함
     '''
-
-    img_names = os.listdir(img_folder)
-
-    # 이미지의 개수와 라벨의 개수가 같은지 체크
-    if len(img_names) > 1 and len(child_names) == 1:
-        child_names = child_names * len(img_names)
-    elif len(img_names) == 0:
-        return
-    elif len(img_names) != len(child_names):
-        raise Exception('Number of images and labels are different.')
 
     # 얼굴 정보를 저장 할 파일
     data_path = os.path.join(data_folder, class_name) + '.pkl'
@@ -44,68 +49,37 @@ def save_face_par(img_folder:str, child_names:list, data_folder:str, class_name:
         with open(data_path,'rb') as f:
             data_dict = pickle.load(f)
     else:
-        data_dict = {'known_img_paths':[], 'known_child_names':[],
+        data_dict = {'known_base64_string':[], 'known_child_ids':[],
                      'known_face_locations':[], 'known_face_encodings':[]}
 
-    # 각 사진마다 얼굴 위치 및 특징 추출
-    for i, img_name in enumerate(img_names):
-        img_path = os.path.join(img_folder, img_name)
-        if img_path in data_dict['known_img_paths']:
-            continue
+    # 사진에서 얼굴 위치 및 특징 추출
+    if base64_string in data_dict['known_base64_string']:
+        return data_dict['known_base64_string'], data_dict['known_child_ids'], data_dict['known_face_locations'], data_dict['known_face_encodings']
+    
+    with open(img_path, 'rb') as img:
+        base64_string = base64.b64encode(img.read())
+    
+    face_location, face_encoding = face_detection(base64_string, upsample=1)
+    
+    if len(face_location) != 1:
+        return data_dict['known_base64_string'], data_dict['known_child_ids'], data_dict['known_face_locations'], data_dict['known_face_encodings']
 
-        face_location, face_encoding = face_detection(img_path, upsample=1)
-        if len(face_location) != 1:
-            continue  
-
-        data_dict['known_img_paths'].append(img_path)
-        data_dict['known_child_names'].append(child_names[i])
-        data_dict['known_face_locations'].append(face_location[0])
-        data_dict['known_face_encodings'].append(face_encoding[0])
+    data_dict['known_base64_string'].append(base64_string)
+    data_dict['known_child_ids'].append(child_ids)
+    data_dict['known_face_locations'].append(face_location[0])
+    data_dict['known_face_encodings'].append(face_encoding[0])
         
     with open(data_path,'wb') as f:
         pickle.dump(data_dict, f)
 
-    return data_dict['known_img_paths'], data_dict['known_child_names'], data_dict['known_face_locations'], data_dict['known_face_encodings']
+    return data_dict['known_base64_string'], data_dict['known_child_ids'], data_dict['known_face_locations'], data_dict['known_face_encodings']
 
 
-def save_face_tea(img_path:str, face_locations_tag:list, child_names_tag:list, data_folder:str, class_name:str):
-    '''
-    선생님이 직접 태깅한 정보를 데이터 베이스에 저장
-    한 장의 이미지에서 여러개 얼굴 태깅 가능
-    len(face_locations_tag) == len(child_names_tag)
-    '''
-    if len(face_locations_tag) != len(child_names_tag):
-        raise Exception('len(face_locations_tag) != len(child_names_tag)')
-
-    face_locations, face_encodings = face_detection(img_path, upsample=2)
-
-    # 얼굴 정보를 저장 할 파일
-    data_path = os.path.join(data_folder, class_name) + '.pkl'
-    if os.path.isfile(data_path):
-        with open(data_path,'rb') as f:
-            data_dict = pickle.load(f)
-    else:
-        data_dict = {'known_img_paths':[], 'known_child_names':[],
-                     'known_face_locations':[], 'known_face_encodings':[]}
-    
-    for i, child_name in enumerate(child_names_tag):
-        if (child_name) and (face_locations_tag[i] not in data_dict['known_face_locations']) and (face_locations_tag[i] in face_locations):
-            data_dict['known_img_paths'].append(img_path)
-            data_dict['known_child_names'].append(child_name)
-            data_dict['known_face_locations'].append(face_locations_tag[i])
-            data_dict['known_face_encodings'].append(face_encodings[face_locations.index(face_locations_tag[i])])
-    
-    with open(data_path,'wb') as f:
-        pickle.dump(data_dict, f)
-
-    return data_dict['known_img_paths'], data_dict['known_child_names'], data_dict['known_face_locations'], data_dict['known_face_encodings']
-
-
-def remove_face(img_path:str, face_locations:list, child_names:list, data_folder:str, class_name:str):
+def remove_face(base64_string:bytes, face_locations:list, child_ids:list, data_folder:str, class_name:str):
     '''저장된 데이터에서 얼굴 정보 삭제'''
 
-    if len(face_locations) != len(child_names):
-        raise Exception('len(face_locations_tag) != len(child_names_tag)')
+    if len(face_locations) != len(child_ids):
+        raise Exception('len(face_locations_tag) != len(child_ids_tag)')
 
     data_path = os.path.join(data_folder, class_name) + '.pkl'
     if not os.path.isfile(data_path):
@@ -115,10 +89,10 @@ def remove_face(img_path:str, face_locations:list, child_names:list, data_folder
         data_dict = pickle.load(f)
 
     for i, face_location in enumerate(face_locations):
-        if (img_path in data_dict['known_img_paths']) and (face_location in data_dict['known_face_locations']) and (child_names[i] in data_dict['known_child_names']):
+        if (base64_string in data_dict['known_base64_string']) and (face_location in data_dict['known_face_locations']) and (child_ids[i] in data_dict['known_child_ids']):
             idx = data_dict['known_face_locations'].index(face_location)
-            del data_dict['known_img_paths'][idx]
-            del data_dict['known_child_names'][idx]
+            del data_dict['known_base64_string'][idx]
+            del data_dict['known_child_ids'][idx]
             del data_dict['known_face_locations'][idx]
             del data_dict['known_face_encodings'][idx]
     
@@ -126,10 +100,10 @@ def remove_face(img_path:str, face_locations:list, child_names:list, data_folder
         pickle.dump(data_dict, f)
         
 
-def face_matching(img_path:str, known_face_encodings:list):
+def face_matching(base64_string:bytes, known_face_encodings:list):
     '''단체 사진에서 검출된 얼굴에 대해 이름 태깅'''
 
-    face_locations, face_encodings = face_detection(img_path, upsample=2)
+    face_locations, face_encodings = face_detection(base64_string, upsample=2)
 
     face_names = []
     for face_encoding in face_encodings:
@@ -140,18 +114,17 @@ def face_matching(img_path:str, known_face_encodings:list):
 
         name = None
         if True in match:
-            name = known_child_names[match.index(True)]
+            name = known_child_ids[match.index(True)]
 
         face_names.append(name)
     return face_locations, face_names
 
 
-def visualize(img_dir:str, img_name:str, output_dir:str, face_locations:list, child_names:list):
+def visualize(base64_string:bytes, output_dir:str, img_name:str, face_locations:list, child_ids:list):
     '''단체 사진에서 얼굴 검출 및 매칭 결과를 이미지로 저장'''
 
-    img_path = os.path.join(img_dir, img_name)
-    bgr_img = cv2.imread(img_path)
-    for (top, right, bottom, left), name in zip(face_locations, child_names):
+    bgr_img = load_base64(base64_string, mode='BGR')
+    for (top, right, bottom, left), name in zip(face_locations, child_ids):
         if not name:
             cv2.rectangle(bgr_img, (left, top), (right, bottom), (255, 0, 0), 2)
             continue
@@ -174,23 +147,31 @@ if __name__ == '__main__':
     
     os.makedirs(group_output_dir, exist_ok=True)
 
-    single_child_names = os.listdir(single_img_dir)
+    single_child_ids = os.listdir(single_img_dir)
     group_img_names = os.listdir(group_img_dir)
 
     tolerance = 0.39
 
     print('=== face detection and save data ===')
-    for child_name in tqdm(single_child_names):
-        single_child_path = os.path.join(single_img_dir, child_name)
+    for child_id in tqdm(single_child_ids):
+        single_child_path = os.path.join(single_img_dir, child_id)
+        img_names = os.listdir(single_child_path)
 
-        known_img_paths, known_child_names, known_face_locations, known_face_encodings = save_face_par(single_child_path, [child_name], './', 'C1')
+        for i, img_name in enumerate(img_names):
+            img_path = os.path.join(single_child_path, img_name)
+
+            with open(img_path, 'rb') as img:
+                base64_string = base64.b64encode(img.read())
+
+            known_base64_string, known_child_ids, known_face_locations, known_face_encodings = save_face_par(base64_string, child_id, './', 'C1')
 
     print('=== face matching ===')
     for img_name in tqdm(group_img_names):
         img_path = os.path.join(group_img_dir, img_name)
 
-        
-        face_locations, face_names = face_matching(img_path, known_face_encodings)
+        with open(img_path, 'rb') as img:
+            base64_string = base64.b64encode(img.read())
+        face_locations, face_names = face_matching(base64_string, known_face_encodings)
 
         if face_names:
-            visualize(group_img_dir, img_name, group_output_dir, face_locations, face_names)
+            visualize(base64_string, group_output_dir, img_name, face_locations, face_names)
